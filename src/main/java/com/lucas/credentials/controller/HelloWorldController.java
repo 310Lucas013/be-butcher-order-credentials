716353a098d740de2ee8a6ab7a33c0650d3209ec
Luuk Vermeer <l.vermeer@student.fontys.nl>
2021-04-07 10:39:40 +0200

    latest changes

--- src/main/java/com/lucas/credentials/controller/HelloWorldController.java
+++ src/main/java/com/lucas/credentials/controller/HelloWorldController.java
@@ -10,24 +10,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin()
 public class HelloWorldController {
 
-//    @Autowired
-//    private AmqpTemplate rabbitTemplate;
-//
-//    @Value("${border.rabbitmq.exchange}")
-//    private String exchange;
-//    @Value("${border.rabbitmq.routingkey}")
-//    private String routingkey;
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
 
-//    @GetMapping(value = "/send-user", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity sendUser() {
-//        UserDao userDao = new UserDao();
-//        userDao.setEmail("test.email@email.nl");
-//        userDao.setStatus(CredentialStatus.UNVERIFIED);
-//        rabbitTemplate.convertAndSend(exchange, routingkey, userDao);
-//        return ResponseEntity.ok().build();
-//    }
+    @Value("${border.rabbitmq.exchange}")
+    private String exchange;
+    @Value("${border.rabbitmq.routingkey}")
+    private String routingkey;
+
+    @GetMapping(value = "/send-user", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity sendUser() {
+        UserDao userDao = new UserDao();
+        userDao.setEmail("test.email@email.nl");
+        userDao.setStatus(CredentialStatus.UNVERIFIED);
+        rabbitTemplate.convertAndSend(exchange, routingkey, userDao);
+        return ResponseEntity.ok().build();
+    }
 
 }
