fac787caa9eb6abe201ef4f5ff95d7980478c30a
Luuk Vermeer <l.vermeer@student.fontys.nl>
2021-04-12 14:25:15 +0200

    customer creation with rabbitmq works.

--- src/main/java/com/lucas/credentials/controller/JwtAuthenticationController.java
+++ src/main/java/com/lucas/credentials/controller/JwtAuthenticationController.java
@@ -1,12 +1,15 @@
 package com.lucas.credentials.controller;
 
+import com.google.gson.Gson;
 import com.lucas.credentials.config.JwtTokenUtil;
+import com.lucas.credentials.messaging.CreateCustomerMessage;
 import com.lucas.credentials.models.JwtRequest;
 import com.lucas.credentials.models.JwtResponse;
 import com.lucas.credentials.models.UserDao;
 import com.lucas.credentials.models.UserDto;
 import com.lucas.credentials.service.JwtUserDetailsService;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +23,21 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/credentials")
 public class JwtAuthenticationController {
     private final AuthenticationManager authenticationManager;
 
     private final JwtTokenUtil jwtTokenUtil;
 
     private final JwtUserDetailsService userDetailsService;
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    @Value("${border.rabbitmq.exchange}")
+    private String exchange;
+    @Value("${border.rabbitmq.routingkey}")
+    private String routingkey;
 
     public JwtAuthenticationController(AuthenticationManager authenticationManager,
                                        JwtTokenUtil jwtTokenUtil, JwtUserDetailsService userDetailsService
@@ -50,7 +62,17 @@ public class JwtAuthenticationController {
     @PostMapping(value = "/register")
     public ResponseEntity<?> saveUser(@RequestBody UserDto user) throws Exception {
         System.out.println(user);
-        return ResponseEntity.ok(userDetailsService.save(user));
+        UserDao userDao = userDetailsService.save(user);
+        Gson gson = new Gson();
+        CreateCustomerMessage ccm = new CreateCustomerMessage();
+        ccm.setCredentialsId(userDao.getId());
+        ccm.setFirstName(user.getFirstName());
+        ccm.setMiddleName(user.getMiddleName());
+        ccm.setLastName(user.getLastName());
+        ccm.setPhoneNumbers(user.getPhoneNumbers());
+        String result = gson.toJson(ccm);
+        rabbitTemplate.convertAndSend(exchange, "create-customer", result);
+        return ResponseEntity.ok().build();
     }
 
     private void authenticate(String username, String password) throws Exception {
--- src/main/java/com/lucas/credentials/models/UserDto.java
+++ src/main/java/com/lucas/credentials/models/UserDto.java
@@ -1,12 +1,22 @@
 package com.lucas.credentials.models;
 
+import java.util.List;
+
 public class UserDto {
     private String email;
     private String password;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private List<String> phoneNumbers;
 
-    public UserDto(String email, String password) {
+    public UserDto(String email, String password, String firstName, String middleName, String lastName, List<String> phoneNumbers) {
         this.email = email;
         this.password = password;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.phoneNumbers = phoneNumbers;
     }
 
     public String getEmail() {
@@ -23,5 +33,37 @@ public class UserDto {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<String> getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public void setPhoneNumbers(List<String> phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
     }
 }
